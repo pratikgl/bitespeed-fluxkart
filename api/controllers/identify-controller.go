daca78193d9964a536dc0f358f566227856328ca
pratikgl <pratikgoyal2712@gmail.com>
2023-09-28 15:10:37 +0530

    added changes

--- api/controllers/identify-controller.go
+++ api/controllers/identify-controller.go
@@ -1,9 +1,15 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/pratikgl/bitespeed-fluxkart/api/models"
+	"github.com/pratikgl/bitespeed-fluxkart/pkg/utils"
 )
 
 func Identify(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Identify"))
+	NewUser := &models.Contact{}
+	utils.ParseBody(r, NewUser)
+	fmt.Println(r.Body, NewUser)
 }
