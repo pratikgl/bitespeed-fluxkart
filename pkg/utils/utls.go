9dba64e658541b7155c1dfffebde617b4dcb5ffd
pratikgl <pratikgoyal2712@gmail.com>
2023-09-28 17:33:41 +0530

    minor change

--- pkg/utils/utls.go
+++ pkg/utils/utls.go
@@ -9,7 +9,7 @@ import (
 // ParseBody is a helper function which is intended for
 // parsing the JSON request body of an HTTP request and
 // unmarshaling it into a Go struct or interface.
-func ParseBody(r *http.Request, x interface{}) {
+func ParseRequestBody(r *http.Request, x interface{}) {
 	// Read the body of the request.
 	if body, err := io.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
