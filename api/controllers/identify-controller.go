edf5a376bdaff68aa7e74f7236c0aa929c3ed025
pratikgl <pratikgoyal2712@gmail.com>
2023-09-28 17:00:18 +0530

    added indetifyrequest and identifyresponse struct

--- api/controllers/identify-controller.go
+++ api/controllers/identify-controller.go
@@ -8,8 +8,26 @@ import (
 	"github.com/pratikgl/bitespeed-fluxkart/pkg/utils"
 )
 
+// IdentifyRequest is the request body for the Identify endpoint.
+type IdentifyRequest struct {
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
+}
+
+// IdentifyResponse is the response body for the Identify endpoint.
+type IdentifyResponse struct {
+	Contact struct {
+		PrimaryContactId    uint     `json:"primaryContactId"`
+		Emails              []string `json:"emails"`
+		PhoneNumbers        []string `json:"phoneNumbers"`
+		SecondaryContactIds []uint   `json:"secondaryContactIds"`
+	} `json:"contact"`
+}
+
 func Identify(w http.ResponseWriter, r *http.Request) {
-	NewUser := &models.Contact{}
-	utils.ParseBody(r, NewUser)
-	fmt.Println(r.Body, NewUser)
+	// printing the request body
+	fmt.Println("Request Body:", r.Body)
+	lola := &models.Contact{}
+	utils.ParseBody(r, lola)
+	fmt.Println("Lola:", lola, *lola.Email, *lola.PhoneNumber)
 }
